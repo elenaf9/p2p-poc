57c57d94336e6241fc0c927c6a03b5a0c743ae25
Elena Frank <elena.frank@cgi.com>
2020-09-15 02:27:56 +0200

    rearranged functions in main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,53 @@ use std::{
     task::{Context, Poll},
 };
 
+// We create a custom network behaviour that combines Kademlia and mDNS.
+#[derive(NetworkBehaviour)]
+struct P2PNetworkBehaviour {
+    kademlia: Kademlia<MemoryStore>,
+    mdns: Mdns,
+}
+
+impl NetworkBehaviourEventProcess<MdnsEvent> for P2PNetworkBehaviour {
+    // Called when `mdns` produces an event.
+    fn inject_event(&mut self, event: MdnsEvent) {
+        if let MdnsEvent::Discovered(list) = event {
+            for (peer_id, multiaddr) in list {
+                self.kademlia.add_address(&peer_id, multiaddr);
+            }
+        }
+    }
+}
+
+impl NetworkBehaviourEventProcess<KademliaEvent> for P2PNetworkBehaviour {
+    // Called when `kademlia` produces an event.
+    fn inject_event(&mut self, message: KademliaEvent) {
+        match message {
+            /* KademliaEvent::RoutingUpdated {
+                peer, addresses, ..
+            } => {
+                println!("peer: {:?}, added address: {:?} ", peer, addresses.into_vec().last().unwrap()),
+            }*/
+            KademliaEvent::QueryResult { result, .. } => match result {
+                QueryResult::GetClosestPeers(Ok(GetClosestPeersOk { key: _, peers })) => {
+                    println!("closest peers: {:?}", peers);
+                    for peer in peers {
+                        let addr_vec = &self.mdns.addresses_of_peer(&peer);
+                        let address = addr_vec.iter().last().unwrap();
+                        println!(
+                            "I wanna say Hi to peer {:?} o: {:?}, but I dont know how",
+                            peer, address
+                        );
+                        Swarm::dial_addr(&self, )
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Create a random PeerId
     let local_keys = identity::Keypair::generate_ed25519();
@@ -34,52 +81,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         .upgrade(upgrade::Version::V1)
         .authenticate(noise)
         .multiplex(yamux);
-
-    // We create a custom network behaviour that combines Kademlia and mDNS.
-    #[derive(NetworkBehaviour)]
-    struct P2PNetworkBehaviour {
-        kademlia: Kademlia<MemoryStore>,
-        mdns: Mdns,
-    }
-
-    impl NetworkBehaviourEventProcess<MdnsEvent> for P2PNetworkBehaviour {
-        // Called when `mdns` produces an event.
-        fn inject_event(&mut self, event: MdnsEvent) {
-            if let MdnsEvent::Discovered(list) = event {
-                for (peer_id, multiaddr) in list {
-                    self.kademlia.add_address(&peer_id, multiaddr);
-                }
-            }
-        }
-    }
-
-    impl NetworkBehaviourEventProcess<KademliaEvent> for P2PNetworkBehaviour {
-        // Called when `kademlia` produces an event.
-        fn inject_event(&mut self, message: KademliaEvent) {
-            match message {
-                /* KademliaEvent::RoutingUpdated {
-                    peer, addresses, ..
-                } => {
-                    println!("peer: {:?}, added address: {:?} ", peer, addresses.into_vec().last().unwrap()),
-                }*/
-                KademliaEvent::QueryResult { result, .. } => match result {
-                    QueryResult::GetClosestPeers(Ok(GetClosestPeersOk { key: _, peers })) => {
-                        println!("closest peers: {:?}", peers);
-                        for peer in peers {
-                            let addr_vec = &self.mdns.addresses_of_peer(&peer);
-                            let address = addr_vec.iter().last().unwrap();
-                            println!(
-                                "I wanna say Hi to peer {:?} o: {:?}, but I dont know how",
-                                peer, address
-                            );
-                        }
-                    }
-                    _ => {}
-                },
-                _ => {}
-            }
-        }
-    }
 
     // Create a Swarm that establishes connections through the given transport
     // and applies the ping behaviour on each connection.
